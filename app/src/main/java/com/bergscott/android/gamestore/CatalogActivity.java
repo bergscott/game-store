205721646d1bf8b8ac4048bf71b2e7e81791a0d9
bergscott <berg.scott@ymail.com>
2017-02-15 15:03:03 -0600

    implement ProductCursorLoader

--- app/src/main/java/com/bergscott/android/gamestore/CatalogActivity.java
+++ app/src/main/java/com/bergscott/android/gamestore/CatalogActivity.java
@@ -1,13 +1,73 @@
 package com.bergscott.android.gamestore;
 
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
-public class CatalogActivity extends AppCompatActivity {
+import com.bergscott.android.gamestore.data.GameStoreContract.ProductEntry;
+
+
+public class CatalogActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    /** ListView containing the list of products in the database */
+    ListView mProductsListView;
+
+    /** Cursor Adapter for the Product information from database */
+    ProductCursorAdapter mProductCursorAdapter;
+
+    /** Product Cursor Loader ID */
+    private static final int PRODUCT_LOADER = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+
+        // find the list view
+        mProductsListView = (ListView) findViewById(R.id.list_view_game);
+
+        // setup the cursor adapter and set it to the list view
+        mProductCursorAdapter = new ProductCursorAdapter(this, null);
+        mProductsListView.setAdapter(mProductCursorAdapter);
+
+        // initialize the cursor loader for product data
+        getLoaderManager().initLoader(PRODUCT_LOADER, null, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        // get the URI for the products table in the database
+        Uri uri = ProductEntry.CONTENT_URI;
+
+        // setup the projection retrieving only the ID, Name, Quantity, and Price of the products
+        String[] projection = {
+                ProductEntry._ID,
+                ProductEntry.COLUMN_PRODUCT_NAME,
+                ProductEntry.COLUMN_PRODUCT_QUANTITY,
+                ProductEntry.COLUMN_PRODUCT_PRICE
+        };
+
+        // return the new cursor loader
+        return new CursorLoader(this, uri, projection, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        // swap the adapter's cursor to the newly loaded cursor
+        mProductCursorAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // adapter's data needs to be cleared, so wap in null and drop all reference to the previous
+        // cursor to prevent memory leaks
+        mProductCursorAdapter.swapCursor(null);
     }
 }
---
+++ app/src/main/java/com/bergscott/android/gamestore/ProductCursorAdapter.java
@@ -0,0 +1,52 @@
+package com.bergscott.android.gamestore;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v4.widget.CursorAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bergscott.android.gamestore.data.GameStoreContract;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by bergs on 2/15/2017.
+ */
+
+public class ProductCursorAdapter extends CursorAdapter {
+
+    public ProductCursorAdapter(Context context, Cursor c) {
+        super(context, c, 0);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.list_item_game, parent, false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+        // find the text view for the name and set it to the value contained in the cursor
+        TextView nameTextView = (TextView) view.findViewById(R.id.name_text_view);
+        nameTextView.setText(cursor.getString(cursor.getColumnIndex(
+                GameStoreContract.ProductEntry.COLUMN_PRODUCT_NAME)));
+
+        // find the text view for the quantity and set it to the value contained in the cursor
+        TextView quantityTextView = (TextView) view.findViewById(R.id.quantity_text_view);
+        int quantity = cursor.getInt(cursor.getColumnIndex(
+                GameStoreContract.ProductEntry.COLUMN_PRODUCT_QUANTITY));
+        quantityTextView.setText(context.getString(R.string.list_item_quantity, quantity));
+
+        // find the text view for the price and set it to the value contained in the cursor
+        TextView priceTextView = (TextView) view.findViewById(R.id.price_text_view);
+        int priceInCents = cursor.getInt(cursor.getColumnIndex(
+                GameStoreContract.ProductEntry.COLUMN_PRODUCT_PRICE));
+        BigDecimal price = new BigDecimal(priceInCents).movePointLeft(2);
+        priceTextView.setText(context.getString(R.string.list_item_price, price));
+
+    }
+}
