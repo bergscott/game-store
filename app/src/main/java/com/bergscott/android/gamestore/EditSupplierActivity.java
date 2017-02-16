3e4e2da5ba87ec376850e7b29b168f3397aa7b55
bergscott <berg.scott@ymail.com>
2017-02-16 09:21:02 -0600

    add EditSupplierActivity and implement basic layout

---
+++ app/src/main/java/com/bergscott/android/gamestore/EditSupplierActivity.java
@@ -0,0 +1,13 @@
+package com.bergscott.android.gamestore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditSupplierActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_supplier);
+    }
+}
