c587c7cffe5e2e2ae3aaa22ce2cf5c393e33e51f
bergscott <berg.scott@ymail.com>
2017-02-21 17:53:54 -0600

    implement sale button in CatalogActivity

--- app/src/main/java/com/bergscott/android/gamestore/ProductCursorAdapter.java
+++ app/src/main/java/com/bergscott/android/gamestore/ProductCursorAdapter.java
@@ -1,12 +1,17 @@
 package com.bergscott.android.gamestore;
 
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
 import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bergscott.android.gamestore.data.GameStoreContract;
 
@@ -28,7 +33,7 @@ public class ProductCursorAdapter extends CursorAdapter {
     }
 
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public void bindView(View view, final Context context, Cursor cursor) {
 
         // find the text view for the name and set it to the value contained in the cursor
         TextView nameTextView = (TextView) view.findViewById(R.id.name_text_view);
@@ -37,7 +42,7 @@ public class ProductCursorAdapter extends CursorAdapter {
 
         // find the text view for the quantity and set it to the value contained in the cursor
         TextView quantityTextView = (TextView) view.findViewById(R.id.quantity_text_view);
-        int quantity = cursor.getInt(cursor.getColumnIndex(
+        final int quantity = cursor.getInt(cursor.getColumnIndex(
                 GameStoreContract.ProductEntry.COLUMN_PRODUCT_QUANTITY));
         quantityTextView.setText(context.getString(R.string.list_item_quantity, quantity));
 
@@ -47,5 +52,30 @@ public class ProductCursorAdapter extends CursorAdapter {
                 GameStoreContract.ProductEntry.COLUMN_PRODUCT_PRICE));
         priceTextView.setText(context.getString(R.string.list_item_price,
                 ProductUtils.getDecimalPrice(priceInCents)));
+
+        final int rowId = cursor.getInt(cursor.getColumnIndex(GameStoreContract.ProductEntry._ID));
+
+        // find the sale button and set its onClickListener
+        Button saleButton = (Button) view.findViewById(R.id.sale_button);
+        saleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (quantity == 0) {
+                    Toast.makeText(context, "No quantity for sale", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Uri uri = ContentUris.withAppendedId(
+                        GameStoreContract.ProductEntry.CONTENT_URI, rowId);
+                ContentValues values = new ContentValues();
+                values.put(GameStoreContract.ProductEntry.COLUMN_PRODUCT_QUANTITY, quantity - 1);
+
+                int rowsUpdated = context.getContentResolver().update(uri, values, null, null);
+
+                if (rowsUpdated == 0) {
+                    Toast.makeText(context, "Problem updating database", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
